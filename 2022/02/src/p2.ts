e54717948b8a083c1ae6a035877ef7183b277cbe
Tomas Pavlatka <tomas.pavlatka@zolar.de>
2023-10-23 12:14:44 +0200

    adding more code

--- 2022/02/src/p2.ts
+++ 2022/02/src/p2.ts
@@ -1,21 +1,92 @@
 import * as fs from 'fs';
 
 export const p2 = (input: string): number => {
-    const calories = Array<number>();
-    let current: number = 0;
+    const rounds = input.split(/\r?\n/);
 
-    const lines = input.split(/\r?\n/);
-    for (const line of lines) {
-        if (line.length === 0) {
-            calories.push(current);
-            current = 0;
-        } else {
-            current += parseInt(line);
+    let score = 0;
+    rounds.forEach((round: string) => {
+        if (round.length !== 0) {
+            const [elf, me] = round.split(' ');
+
+            const myOption = mapping(me, elf);
+            score += result(elf, myOption) + myHand(myOption);
         }
-    } 
+    });
 
-    return calories.sort((a, b) => a - b).reverse().slice(0, 3).reduce((a, b) => a + b);
+    return score;
+}
+
+const mapping = (me: string, elf: string): string => {
+    if (me === 'X') {
+        switch (elf) {
+            case 'A':
+                return 'Z';
+            case 'B':
+                return 'X';
+            default:
+                return 'Y';
+        }
+    } else if (me === 'Y') {
+        switch (elf) {
+            case 'A':
+                return 'X';
+            case 'B':
+                return 'Y';
+            default:
+                return 'Z';
+        }
+    } else {
+        switch (elf) {
+            case 'A':
+                return 'Y';
+            case 'B':
+                return 'Z';
+            default:
+                return 'X';
+        }
+    }
+}
+
+const myHand = (me: string): number => {
+    switch (me) {
+        case 'X':
+            return 1;
+        case 'Y':
+            return 2;
+        default:
+            return 3;
+    }
+}
+
+const result = (elf: string, me: string): number => {
+    if (me === 'X') {
+        switch (elf) {
+            case 'A':
+                return 3;
+            case 'B':
+                return 0;
+            default:
+                return 6;
+        }
+    } else if (me === 'Y') {
+        switch (elf) {
+            case 'A':
+                return 6;
+            case 'B':
+                return 3;
+            default:
+                return 0;
+        }
+    } else {
+        switch (elf) {
+            case 'A':
+                return 0;
+            case 'B':
+                return 6;
+            default:
+                return 3;
+        }
+    }
 }
 
 console.log(p2(fs.readFileSync('./src/input', 'utf8')));
-
