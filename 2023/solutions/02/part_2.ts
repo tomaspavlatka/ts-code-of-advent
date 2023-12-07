ea3e83a157a04fce27244ba2bdf7511fc3cd4f27
Tomas Pavlatka <tomas.pavlatka@zolar.de>
2023-12-07 11:44:20 +0100

    solution for day 2

--- 2023/solutions/02/part_2.ts
+++ 2023/solutions/02/part_2.ts
@@ -1,7 +1,34 @@
 import { task } from "../../utils/task";
 
 export const p2 = (input: string): number => {
-    return 0;
+    return input
+        .trim()
+        .split("\n")
+        .map(line => { 
+                const map = new Map<string, number>();
+
+                line.split(":")[1]
+                    .trim()
+                    .split(';')
+                    .map(round => {
+                        round.trim().split(",").forEach(item => {
+                            const [count, color] = item.trim().split(" ");
+                            if ((map.get(color) || 0) < parseInt(count)) {
+                                map.set(color, parseInt(count));
+                            }
+                        })
+
+                        return map;
+                    });
+
+                let power = 1;
+                map.forEach((value: number) => {
+                    power *= value;
+                });
+
+                return power;
+        })
+        .reduce((acc, curr) => acc + curr, 0) || 0;
 }
 
 task(p2, 2, 2);
