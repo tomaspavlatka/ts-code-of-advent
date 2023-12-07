90a763c00349e4b2d3e09608a32aa489c4cc74b4
Tomas Pavlatka <tomas.pavlatka@zolar.de>
2023-12-07 09:52:42 +0100

    fixing 2nd part of day 1

--- 2023/solutions/01/part_2.ts
+++ 2023/solutions/01/part_2.ts
@@ -1,39 +1,19 @@
+import { isNumeric } from "../../utils/string";
 import { task } from "../../utils/task";
 
-export const p2 = (input: string): number => {
-    let sum = 0;
-    input.split("\n").forEach( line => {
-        sum += parseLineNumber(line);
-    });
-    return sum;
+export const p2 = (input: string): Number => {
+    const letterNumbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+    return input
+        .trim()
+        .split("\n")
+        .map(line => {
+            return [...line.matchAll(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g)]
+                .map(match => match[1])
+                .map(match => isNumeric(match) ? Number(match) : letterNumbers.indexOf(match) + 1)
+        })
+        .map(data => new Number(`${data[0]}${data[data.length-1]}`))
+        .reduce((acc, curr) => acc + curr, 0);
 }
-
-const parseLineNumber = (input: string): number => {
-    const matches = input.match(/(\d|one|two|three|four|five|six|seven|eight|nine)/g);
-    if (!matches) {
-        return 0;
-    }
-
-    return 10 * convertToRealNumber(matches[0]) + convertToRealNumber(matches[matches.length-1]);
-}
-
-const convertToRealNumber = (input: string): number => {
-    let map = new Map<string, number>([
-        ["one", 1],
-        ["two", 2],
-        ["three", 3],
-        ["four", 4],
-        ["five", 5],
-        ["six", 6],
-        ["seven", 7],
-        ["eight", 8],
-        ["nine", 9]
-    ]);
-
-    const value = map.get(input);
-
-    return value != undefined ? value : Number(input);
-}
-
 
 task(p2, 1, 2);
---
+++ 2023/utils/string.ts
@@ -0,0 +1,4 @@
+export const isNumeric = (value: any): boolean => {
+  // Love JS
+  return !isNaN(value)
+}
