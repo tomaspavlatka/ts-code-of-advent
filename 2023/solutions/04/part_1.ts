ae7d1caf0e4d23a52663ee7669ca0329723baba1
Tomas Pavlatka <tomas.pavlatka@zolar.de>
2023-12-20 09:25:01 +0100

    day 4, part 1

---
+++ 2023/solutions/04/part_1.ts
@@ -0,0 +1,23 @@
+import { task } from "../../utils/task";
+
+export const p1 = (input: string): number => {
+    return input.split("\n")
+        .map((line) => {
+        const [_, numbers] = line.split(":");
+
+        const [winning, mine] = numbers.split("|").map((n) => n.trim());
+
+        // the puzzle contains usually 2 places for a number, 5 is not `5` but ` 5`
+        const winningNumbers = winning.split(" ").map((n) => n.trim()).filter((n) => n.length > 0);
+
+        return mine
+            .split(" ")
+            .map((n) => n.trim())
+            .filter((n) => winningNumbers.includes(n))
+            .map((n) => {console.log(n); return n})
+            .reduce((acc) => acc === 0 ? 1 : acc * 2, 0);
+        })
+        .reduce((acc, n) => acc + n, 0);
+}
+
+task(p1, 4, 1);
---
+++ 2023/solutions/04/part_2.ts
@@ -0,0 +1,7 @@
+import { task } from "../../utils/task";
+
+export const p2 = (input: string): number => {
+    return 0;
+}
+
+task(p2, 04, 2);
